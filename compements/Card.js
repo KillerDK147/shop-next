9200bb552d8b75927e7678ce975d4e6f8b801677
matgl <gamermatdk@gmail.com>
2022-04-15 13:28:30 +0200

    commit

---
+++ compements/Card.js
@@ -0,0 +1,25 @@
+import { Button, Card as BsCard } from "react-bootstrap";
+
+const Card = ({ titel, katergori, besk, sti, antal, enhed, pris }) => {
+  console.log(titel);
+  return (
+    <BsCard>
+      <div className="p-2">
+        <BsCard.Img variant="top" src={sti} />
+      </div>
+      <BsCard.Body>
+        <BsCard.Title>
+          {titel} - pris: {pris},-
+        </BsCard.Title>
+        <BsCard.Text>{besk}</BsCard.Text>
+        <BsCard.Text>
+          {antal} {enhed}
+        </BsCard.Text>
+        <Button className="btn btn-succses">Resver nu</Button>
+      </BsCard.Body>
+      <BsCard.Header className="small">{katergori}</BsCard.Header>
+    </BsCard>
+  );
+};
+
+export default Card;
---
+++ pages/home.js
@@ -0,0 +1,28 @@
+import React from "react";
+import CardBord from "../compements/CardBordGroup";
+import "bootstrap/dist/css/bootstrap.min.css";
+const Home = () => {
+  const cards = [
+    {
+      titel: "Køkken",
+      katergori: "Køkken",
+      besk: "Køkken",
+      sti: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+      antal: "1",
+      enhed: "stk",
+      pris: "100",
+    },
+    {
+      titel: "Køkken",
+      katergori: "Køkken",
+      besk: "Køkken",
+      sti: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+      antal: "1",
+      enhed: "stk",
+      pris: "100",
+    },
+  ];
+  return <CardBord cards={cards} />;
+};
+
+export default Home;
