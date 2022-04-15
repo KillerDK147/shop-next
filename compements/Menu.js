fbf2759af328116e795efa96c4c5a1d8cb010424
matgl <gamermatdk@gmail.com>
2022-04-15 15:14:28 +0200

    test

---
+++ compements/Menu.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import Link from "next/link";
+
+class Menu extends Component {
+  state = {};
+  render() {
+    return (
+      <Navbar bg="dark" expand="lg" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Mr.negativ`s-Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Link href="/">
+                <p>Home</p>
+              </Link>
+              <Link href="/home">
+                <p>Farmer</p>
+              </Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="https://matlovescorona.tk">
+                  matlovescorona
+                </NavDropdown.Item>
+                <NavDropdown.Item href="https://matlovescorona.tk/mettemor">
+                  mette soundbord
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              Log in <></>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+}
+
+export default Menu;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Link from "next/link"
 import Nav from 'react-bootstrap/Nav'
+import Menu from "../compements/Menu"
 export default function Home() {
   return (
     <div>
+      <Menu />
       <h1>Hello Next.js</h1>
       <Link href="/home">
         <a>Home</a>
