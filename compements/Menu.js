7b73f5c990a169a32ed3ab635bb9a3ecc82ab4f4
Mathias Grundvad Laursen <math181r@skivecollege.dk>
2022-04-26 14:12:29 +0200

    added foto

--- compements/Menu.js
+++ compements/Menu.js
@@ -6,36 +6,39 @@ import {
   SSRProvider,
 } from "react-bootstrap";
 import Link from "next/link";
-export default function Home() {
+export default function Home(props) {
   return (
-    <Navbar bg="light" expand="lg">
-      <SSRProvider>
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Link href="/home" passHref>
-                <Nav.Link>Home 2</Nav.Link>
-              </Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </SSRProvider>
-    </Navbar>
+    <div>
+      <Navbar bg="light" expand="lg">
+        <SSRProvider>
+          <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Link href="/home" passHref>
+                  <Nav.Link>Home 2</Nav.Link>
+                </Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </SSRProvider>
+      </Navbar>
+      <main>{props.children}</main>
+    </div>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { useEffect } from "react";
+import Menu from "../compements/Menu";
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     import("bootstrap/dist/js/bootstrap");
   }, []);
-  return <Component {...pageProps} />;
+  return (
+    <Menu>
+      <Component {...pageProps} />
+    </Menu>
+  );
 }
 
 export default MyApp;
