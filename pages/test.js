bcb0c2b060fd5458cd4f62279fd07ebfdbbeeb02
KillerDK147 <58728128+KillerDK147@users.noreply.github.com>
2022-05-11 10:29:10 +0200

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -28,7 +28,7 @@ export default function PrivatePage(props) {
     <div>
       <div>
         <img src={createObjectURL} />
-        <h4>Select Image test2</h4>
+        <h4>Select Image test3</h4>
         <input type="file" name="myImage" onChange={uploadToClient} />
         <button
           className="btn btn-primary"
